1c01cbc98a14e3ff79206736014f88230b1eccc0
Talha Shahid <talhashahidch@gmail.com>
2022-06-30 18:07:35 +0500

    Second Commit

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,65 @@ const cityName = document.getElementById('cityName');
 const temp = document.getElementById('temp');
 const temp_status = document.getElementById('temp_status');
 const city_name = document.getElementById('city_name');
-const data_hide = document.querySelector(".middle_layer")
+const data_hide = document.querySelector(".middle_layer");
+const day = document.getElementById('day')
+const date = document.getElementById('date')
+
+//Day and Date
+
+const getCurrentDay = () => {
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tue";
+  weekday[3] = "Wed";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  let currentTime = new Date();
+  let day = weekday[currentTime.getDay()];
+  return day;
+};
+
+const getCurrentTime = () => {
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  var now = new Date();
+  var month = months[now.getMonth() + 1];
+  var date = now.getDate();
+
+  let hours = now.getHours();
+  let mins = now.getMinutes();
+
+  let periods = "AM";
+
+  if (hours > 11) {
+    periods = "PM";
+    if (hours > 12) hours -= 12;
+  }
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+
+  return `${month} ${date} | ${hours}:${mins}${periods}`;
+};
+
+day.innerHTML = getCurrentDay();
+date.innerHTML= getCurrentTime();
 
 
 //Get Info Function
