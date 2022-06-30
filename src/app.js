d53575f2beb3b5a568a21b69e615a52b8c6a959f
Talha Shahid <talhashahidch@gmail.com>
2022-06-30 18:42:38 +0500

    third Commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 //public static path
  const static_path = path.join(__dirname, "../public");
